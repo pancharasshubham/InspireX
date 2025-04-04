cc5d2163323486c02b7df3449f14d622ab71e2bd
shubh <15013678-shubhpancharas@users.noreply.replit.com>
2025-04-04 17:14:59 +0000

    updated project files

---
+++ models/User.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import next from "next";
+
+export interface IUser {
+    email: string;
+    password: string;
+    _id?: mongoose.Types.ObjectId;
+    createAt?: Date;
+    updateAt?: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true },  
+  },
+    { timestamps: true }
+);
+
+userSchema.pre("save", async function (next){
+  if (this.isModified("password")){
+    this.password = await       bcrypt.hash(this.password, 10);
+}  
+next()
+})
