543a5cf1f298727cb2ad9a4df92ab58e0076821a
pancharasshubham <pancharasshubham@gmail.com>
2026-04-20 13:58:13 +0530

    feat: add MongoDB connection, Video model, and POST /api/videos

---
+++ app/api/videos/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/db";
+import Video from "@/models/Video";
+
+export async function POST(request: Request) {
+  try {
+    await connectDB();
+
+    const body = await request.json();
+
+    const newVideo = await Video.create({
+        title: body.title,
+        videoUrl: body.videoUrl,
+        category: body.category,
+    });
+
+    return NextResponse.json({
+        success: true,
+        message: "Video created successfully",
+        data: newVideo,
+    },
+    { status: 201 }
+    ); 
+
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { success: false, message: "Failed to create video" },
+      { status: 500 }
+    );
+  }
+}
